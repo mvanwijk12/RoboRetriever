2a1eab50a3e0115b02bbeb739331254a6f9c979a
Becleith <becleith@gmail.com>
2024-08-20 21:57:54 +1000

    created main file, fixed statemachine

--- milestone1/statemachine.py
+++ milestone1/statemachine.py
@@ -1,57 +1,36 @@
 # this contains the names of each state and controls how we pass between each state. 
 # made by Bec 20/08/2024
 
-class BallDetect:
-    def on_event(self, event):
-        if event == "trigger_pcontrol":
-            return PControlState()
-        elif event == "arrived_at_ball":
-            return ReturnHome()
-        elif event == "no_ball_detected":
-            return RotateState()
-        else:
-            return self
+class StateMachine():
+    BallDetect = 1
+    RotateState = 2
+    PControlState = 3
+    ReturnHome = 4
 
-class RotateState:
-    def on_event(self, event):
-        if event == "complete":
-            return BallDetect()
-        else:
-            return self
+    currentState = BallDetect
 
+    def __init__(self, initialState):
+        self.currentState = initialState
 
-class PControlState:
-    controller = Controller(0.0008,0,0)
-    # provide to the PID function the error from the centreline. so "pixel_from_left" - centreline
-    error = input("enter error:")
-    out = controller.PID(int(error))
-    lwheel, rwheel = controller.homing_multiplier(out)
-    print('left wheel ', round(lwheel,2), ', right wheel ', round(rwheel,2))
-    robot = Drive()
-    robot.set_1D_direction(dirForward=False)
-    robot.drive(speed=0.2, leftwheel_multilpier=lwheel, rightwheel_multiplier=rwheel)
+    def getState(self):
+        return self.currentState
 
+    def getStateString(self):
+        if(self.currentState == StateMachine.BallDetect):
+            return "BallDetect"
+        elif(self.currentState == StateMachine.RotateState):
+            return "RotateState"
+        elif(self.currentState == StateMachine.PControlState):
+            return "PControlState"
+        elif(self.currentState == StateMachine.ReturnHome):
+            return "ReturnHome"
+        
 
-    def on_event(self, event):
-        if event == "completed":
-            return BallDetect()
-        else:
-            return self
+    def setState(self, state):
+        self.currentState = state
 
-class ReturnHome:
-    def on_event(self, event):
-        return self 
-        # final state
-
-
-# define statemachine
-class StateMachine:
-    def __init__(self):
-        self.state = BallDetect()
-
-    def on_event(self, event):
-        self.state = self.state.on_event(event)
         
 # run this only when this file is run
 if __name__ == "__main__":
-    state_machine = StateMachine()
+    print("Error: This is a class file")
+

4a03fd9603f526800a65ae0df485c73bfefc1060
Matt van Wijk <mvanwijk12@gmail.com>
2024-08-20 22:35:54 +1000

    (upload) timer example upload

--- research/cv/track_example.py
+++ research/cv/track_example.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
 # Load a model
-model = YOLO("yolov8n.pt")  # load an official detection model
+model = YOLO("yolov8s.pt")  # load an official detection model
 
 # Track with the model
-results = model.track(source="tcp://robo-retriever.local:8554", show=True)
+results = model.track(source="https://www.youtube.com/watch?v=sp6LpqPOENc", show=True)

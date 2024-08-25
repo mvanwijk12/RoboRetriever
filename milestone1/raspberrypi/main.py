33332a6d9c7c37d176004226af5d0d238f93ad24
Matt van Wijk <mvanwijk12@gmail.com>
2024-08-25 17:05:01 +1000

    hardware validation 1

--- milestone1/raspberrypi/main.py
+++ milestone1/raspberrypi/main.py
@@ -1,10 +1,10 @@
 # This is the main code
-import pigpio
+#import pigpio
 import time
-import RPi.GPIO as GPIO
+#import RPi.GPIO as GPIO
 from drive import Drive
 from Pcontrol import Controller
-#from server import ConnectionServer
+from server import ConnectionServer
 
 # Constants
 LIMIT_SWITCH_PIN = 10
@@ -27,7 +27,7 @@ class RobotController:
         #GPIO.setup(LIMIT_SWITCH_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
         # Initialize pigpio
-        self.pi = pigpio.pi()
+        #self.pi = pigpio.pi()
 
     def setup_connection(self):
         con = ConnectionServer().start()
@@ -38,37 +38,42 @@ class RobotController:
         # input to controller includes the gains kp, ki, kd. these can be adjusted for tuning
         controller = Controller(0.0008,0,0)
         pixel_error = 0.0
-        PIDout = 0.0
+        PIDout = None
         while True:
             # Fetch data from laptop
             con = self.setup_connection()
             try:
                 x = con.get_message()
                 if x is None:
-                    print("no ball detected, drive straight")
+                    print("no ball detected, driving straight")
                     robot.set_1D_direction(dirForward=False)
-                    # robot.drive(distance=1, speed=0.2)
                     PIDout = 0.0
                 else:
                     pixel_error = x["error"]
                     print("ball detected, control initiated...")
                     # run PID with ball position error to make an adjustment
                     PIDout = controller.PID(float(pixel_error))
-            except:
-                print("Error: Coild not get message from server")
-            # homing takes output of PID 
-            lwheel, rwheel = controller.homing_multiplier(PIDout)
+            
+            self.lwheel, self.rwheel = controller.homing_multiplier(PIDout)
             # save the multiplier for bth wheels together, used for reversing
-            both_wheelsLR = [lwheel, rwheel]
-            stored_pathway.append(both_wheelsLR)
-            # alternative
-            lwheel_sum += lwheel
-            rwheel_sum += rwheel
+            both_wheelsLR = [self.lwheel, self.rwheel]
+            self.stored_pathway.append(both_wheelsLR)
+            print("stored path so far:", self.stored_pathway)
+            # # alternative
+            # lwheel_sum += lwheel
+            # rwheel_sum += rwheel
             print('left wheel ', round(lwheel,2), ', right wheel ', round(rwheel,2))
-            robot = Drive()
+            
             robot.set_1D_direction(dirForward=False)
             robot.drive(distance=0.2, speed=0.2, leftwheel_multilpier=lwheel, rightwheel_multiplier=rwheel)
             #drive slowly slow down 
+                
+            except KeyboardInterrupt:
+            # terminate gracefully
+                robot.pi.hardware_PWM(robot.stepL, 0, 500000)
+                robot.pi.hardware_PWM(robot.stepR, 0, 500000)
+                GPIO.cleanup()
+                sys.exit()
 
             
 
@@ -86,110 +91,3 @@ class RobotController:
 if __name__ == "__main__":
     robot_controller = RobotController()
     robot_controller.run()
-
-
-#     ## fetch from laptop: are there any lines in danger zone?
-#     output from linedetect is an angle 
-#     if yes turn away from line
-#     if angle_line is not None:
-#         if 0 <= angle_line <= 180:
-#             # line is in the right hand side of frame. so turn LEFT
-#             lwheel = 0.5
-#             rwheel = 1.5
-#             both_wheelsLR = [lwheel, rwheel]
-#             stored_pathway.append(both_wheelsLR)
-#             # alternative
-#             lwheel_sum += lwheel
-#             rwheel_sum += rwheel
-#             print('left wheel ', round(lwheel,2), ', right wheel ', round(rwheel,2))
-#             robot = Drive()
-#             robot.set_1D_direction(dirForward=False)
-#             robot.drive(distance=0.2, speed=0.2, leftwheel_multilpier=lwheel, rightwheel_multiplier=rwheel)
-#         else:
-#             # line is in the left hand side of frame. so turn RIGHT
-#             lwheel = 1.5
-#             rwheel = 0.5
-#             both_wheelsLR = [lwheel, rwheel]
-#             stored_pathway.append(both_wheelsLR)
-#             # alternative
-#             lwheel_sum += lwheel
-#             rwheel_sum += rwheel
-#             print('left wheel ', round(lwheel,2), ', right wheel ', round(rwheel,2))
-#             robot = Drive()
-#             robot.set_1D_direction(dirForward=False)
-#             robot.drive(distance=0.2, speed=0.2, leftwheel_multilpier=lwheel, rightwheel_multiplier=rwheel)
-    
-
-#     # Detect when reached target (ball hits limit switch)
-#     #get from limit switch = targetreached = true
-#     if GPIO.input(LIMIT_SWITCH_PIN) == False:
-
-#     if time.time() - start_time >= timeout:
-#         break
-#         drive backwards, retrace our steps
-#         robot = Drive()
-#         robot.set_1D_direction(dirForward=True)
-#         for current_actionLR in stored_pathway:
-#             # run through all the steps we took backwards, setting our left wheel as right and right as left.
-#             robot.drive(speed=0.1, leftwheel_multilpier=current_actionLR[1], rightwheel_multiplier=current_actionLR[0])
-#         break
-#     # fetch error of ball position from the laptop
-    
-#     try:
-#         print(f'MESSAGE IS {con.get_message()}')
-#         x = con.get_message()
-#         if x is not None:
-#             error = x["error"]
-#     except KeyboardInterrupt:
-#         con.close()
-#         print('closing..')
-
-#     if x is None:
-#         print("no ball detected, drive straight")
-#         robot = Drive()
-#         robot.set_1D_direction(dirForward=False)
-#         robot.drive(distance=1, speed=0.2)
-#     else:
-#         print("ball detected, control initiated...")
-#         ### run the pcontrol for one movement
-#         # input to controller includes the gains kp, ki, kd. these can be adjusted for tuning
-#         controller = Controller(0.0008,0,0)
-#         # running PID uses the ball position error to make an adjustment
-#         PIDout = controller.PID(float(error))
-#         # homing takes output of PID 
-#         lwheel, rwheel = controller.homing_multiplier(PIDout)
-#         # save the multiplier for bth wheels together, used for reversing
-#         both_wheelsLR = [lwheel, rwheel]
-#         stored_pathway.append(both_wheelsLR)
-#         # alternative
-#         lwheel_sum += lwheel
-#         rwheel_sum += rwheel
-#         print('left wheel ', round(lwheel,2), ', right wheel ', round(rwheel,2))
-#         robot = Drive()
-#         robot.set_1D_direction(dirForward=False)
-#         robot.drive(speed=0.2, leftwheel_multilpier=lwheel, rightwheel_multiplier=rwheel)
-
-# KeyboardInterrupt:
-#     # terminate gracefully
-#     robot.pi.hardware_PWM(robot.stepL, 0, 500000)
-#     robot.pi.hardware_PWM(robot.stepR, 0, 500000)
-#     GPIO.cleanup()
-#     sys.exit()
-
-
-
-
-    
-#     # or the other wway of doing this
-#     if GPIO.input(LIMIT_SWITCH_PIN) == False:
-#         # drive backwards by the summed proportional amount of each wheel
-#         robot.Drive()
-#         robot.set_1D_direction(dirForward=False)
-#         lwheel_norm = lwheel_sum/rwheel_sum
-#         rwheel_norm = 1
-#         # drive turning by proportional total amount
-#         robot.drive(speed=0.2, leftwheel_multilpier=rwheel_norm, rightwheel_multiplier=lwheel_norm)
-#         travelled_distance = int(stored_pathway.length/2)
-#         # drive straight for the distance travelled.
-#         robot.drive(distance=travelled_distance,speed=0.2, leftwheel_multilpier=0.5, rightwheel_multiplier=0.5)
-

d7db5f086b43c3f54b994cb96b3e10f61eb0b0d8
Alex Jones <ajon037@student.monash.edu>
2024-09-04 20:47:57 +1000

    Line avoidance direction calculation

---
+++ research/angle_reflection.py
@@ -0,0 +1,18 @@
+__author__ = "Alex Jones"
+__date__ = "04/09/2024"
+
+
+import math
+import numpy as np
+
+def angle_converter(line):
+    # Input is line in the form ax+by=c, only uses a and b
+    initial_direction = np.array([0,1])
+    normal_direction = line/np.linalg.norm(line)
+    reflected_direction = initial_direction - (2*np.dot(initial_direction,normal_direction))*normal_direction
+    return reflected_direction/np.linalg.norm(reflected_direction)
+
+if __name__ == "__main__":
+    test_line = np.array([2,3])
+    result = angle_converter(test_line)
+    print(result)

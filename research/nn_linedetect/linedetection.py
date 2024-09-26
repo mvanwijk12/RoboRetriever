57415fed509609007fa02a46162bbe3d7bc5fa2d
John B <jbui0004@student.monash.edu>
2024-09-27 00:29:55 +1000

    Add viz of all lines with diff colours

--- research/nn_linedetect/linedetection.py
+++ research/nn_linedetect/linedetection.py
@@ -1,6 +1,8 @@
 from ultralytics import YOLO
 import numpy as np
 import cv2
+import logging
+import logging.config
 
 def detect_line(masks, box_width_ratio=0.9, box_height_ratio=0.25, bottom_offset_ratio=0.05, orig_img=None):
     # Initialise the trigger box
@@ -56,18 +58,7 @@ def detect_line(masks, box_width_ratio=0.9, box_height_ratio=0.25, bottom_offset
     triggered = False
     largest_line = [None, None]
     x_lines = (None, None, None, None)
-    for line in masks:
-        line_mask = np.array(line.data[0])  # 3D array of 1
-        #print(calc_cover_frame(line_mask))
-        trig_amount = calc_trigger(line_mask)
 
-        if trig_amount is not None:
-            # if the line is in the trigger box
-            triggered = True
-            single_line, x_lines = calc_single_line(line_mask)
-            trig_lines.append((single_line, trig_amount))
-            largest_line = max(trig_lines, key=lambda x: x[1])
-    
     # Perform visualisation if given the original image
     if orig_img is not None:
         # Add a trigger box
@@ -75,15 +66,41 @@ def detect_line(masks, box_width_ratio=0.9, box_height_ratio=0.25, bottom_offset
         bottom_right = (box_x_start + box_width, box_y_start + box_height)
         img_resize = cv2.resize(orig_img, (frame_width, frame_height))
         trig_viz = cv2.rectangle(img_resize, top_left, bottom_right, 255, 2)
-        x1, y1, x2, y2 = x_lines
-        line_viz = cv2.line(trig_viz, (x1, y1), (x2, y2), (0, 0, 255), 2)
 
-        cv2.imshow("Detected Lines with Trigger Box", line_viz)
+    # Go through all lines
+    for line in masks:
+        line_mask = np.array(line.data[0])  # 3D array of 1
+        #print(calc_cover_frame(line_mask))
+        trig_amount = calc_trigger(line_mask)
+        single_line, x_lines = calc_single_line(line_mask)
+
+        # For viz
+        line_col = (0, 255, 0)
+        line_thc = 1
+
+        if trig_amount is not None:
+            # if the line is in the trigger box
+            triggered = True
+            trig_lines.append((single_line, trig_amount))
+            largest_line = max(trig_lines, key=lambda x: x[1])
+
+            # For viz
+            line_col = (0, 0, 255)
+            line_thc = 2
+
+        # Add line as visualisation
+        if orig_img is not None:
+            x1, y1, x2, y2 = x_lines                
+            line_viz = cv2.line(trig_viz, (x1, y1), (x2, y2), line_col, line_thc)
+            cv2.imshow("Detected Lines with Trigger Box", line_viz)
     
     return triggered, largest_line[0]  # formerly trig (bin), angle, distance, np array of a and b
 
 
 if __name__ == "__main__":
+    #logging.config.fileConfig('log.conf')
+    logger = logging.getLogger(__name__)
+
     # folder = "../opencv_tests/court_lines/"
     # image_file_names = ['20240820_124734.jpg','20240820_124438.jpg','20240820_124447.jpg','20240820_124451.jpg','20240820_124509.jpg','20240820_124511.jpg',
     #                     '20240820_124521.jpg','20240820_124524.jpg','20240820_124535.jpg','20240820_124539.jpg','20240820_124551.jpg','20240820_124554.jpg',
